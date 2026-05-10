544d9fea432ed7257c7b04e2ff77951a710c27e4
Shifa Anjani Desha <sanjani462@gmail.com>
2026-05-11 02:32:53 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // Konfigurasi untuk FinTrack AI agar React & Tailwind jalan bareng
 export default defineConfig({
   plugins: [react()],
+  base: '/fintrack-ai/',
 })
